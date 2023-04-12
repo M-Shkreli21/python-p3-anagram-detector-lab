7100061abf360a1001cf40c66b45728a79d2f001
Mark Shkreli <m.shkreli96@icloud.com>
2023-04-12 15:24:25 -0400

    Lab Done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+class Anagram:
+    
+    def __init__(self, word):
+        self.word_letters = sorted([letter for letter in word])
+        
+    def match(self, words):
+        anagram_test = []
+        
+        for word in words:
+            if sorted([letter for letter in word]) == self.word_letters:
+                anagram_test.append(word)
+        
+        return anagram_test
+        
